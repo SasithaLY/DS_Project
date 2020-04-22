adf879185827dd05a4b5b8bf802e27d7c5087331
ishan <agishandissanayake@gmail.com>
2020-04-22 15:44:56 +0530

    Desktop Client Done.

---
+++ Alarm_front_end/src/com/alarm/ui/Login.java
@@ -0,0 +1,364 @@
+package com.alarm.ui;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author ishan
+ */
+public class Login extends javax.swing.JFrame {
+
+    private static String TOKEN_BEARER_TYPE;
+    public static String TOKEN_BEARER;
+    Timer timer = new Timer();
+
+    public Login() throws Exception {
+        initComponents();
+        getAlarmData();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    Date date = new Date();
+                    getAlarmData();
+                    labelLogin.setText("Refreshed! " + formatter.format(date));
+                } catch (Exception ex) {
+                    Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }, 0, 30000);
+    }
+
+    public void getAlarmData() throws Exception {
+
+        String url = "http://localhost:8080/api/getSensors";
+        boolean critical = false;
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+
+        ArrayList<Alarm> alarm = new ArrayList<>();
+        alarm = getList(response.toString());
+
+        DefaultTableModel model = (DefaultTableModel) jTableLogin.getModel();
+        model.setRowCount(0);
+        Object rowData[] = new Object[6];
+        
+        ArrayList<String> criticalAlarmIds = new ArrayList<String>();
+
+        for (int i = 0; i < alarm.size(); i++) {
+            rowData[0] = alarm.get(i).alarmId;
+            rowData[1] = alarm.get(i).status;
+            rowData[2] = alarm.get(i).floorNumber;
+            rowData[3] = alarm.get(i).roomNumber;
+            rowData[4] = alarm.get(i).smokeLevel;
+            rowData[5] = alarm.get(i).co2Level;
+
+            if (alarm.get(i).smokeLevel > 5 || alarm.get(i).co2Level > 5) {
+                criticalAlarmIds.add(String.valueOf(alarm.get(i).alarmId));
+                critical = true;
+            }
+
+            if (critical == true) {
+                alertDisplayLogin.setText("Alert! Alarm ID " + criticalAlarmIds + " is/are in Critical Condition!");
+            } else {
+           
+                alertDisplayLogin.setText("");
+            }
+
+            model.addRow(rowData);
+        }
+
+    }
+
+    public ArrayList<Alarm> getList(String json) {
+
+        ArrayList<Alarm> arrayList = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            for (int count = 0; count < jsonArray.length(); count++) {
+                Alarm alarmObject = new Alarm();
+                JSONObject jsonObject = jsonArray.getJSONObject(count);
+                alarmObject.setAlarmId(jsonObject.getInt("sensorId"));
+                alarmObject.setFloorNumber(jsonObject.getInt("floorNo"));
+                alarmObject.setRoomNumber(jsonObject.getInt("roomNo"));
+                alarmObject.setSmokeLevel(jsonObject.getInt("smoke"));
+                alarmObject.setCo2Level(jsonObject.getInt("co2"));
+                alarmObject.setStatus(jsonObject.getInt("active"));
+
+                arrayList.add(alarmObject);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        usernameInput = new java.awt.TextField();
+        label1 = new java.awt.Label();
+        label2 = new java.awt.Label();
+        passwordInput = new javax.swing.JPasswordField();
+        submit = new java.awt.Button();
+        statusDisplayLogin = new java.awt.Label();
+        jScrollPane8 = new javax.swing.JScrollPane();
+        jTableLogin = new javax.swing.JTable();
+        labelLogin = new java.awt.Label();
+        alertDisplayLogin = new java.awt.Label();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        label1.setForeground(new java.awt.Color(0, 0, 0));
+        label1.setName(""); // NOI18N
+        label1.setText("Username:");
+
+        label2.setForeground(new java.awt.Color(0, 0, 0));
+        label2.setText("Password:");
+
+        submit.setBackground(new java.awt.Color(255, 196, 9));
+        submit.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        submit.setLabel("Login");
+        submit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitActionPerformed(evt);
+            }
+        });
+
+        statusDisplayLogin.setBackground(new java.awt.Color(255, 255, 255));
+        statusDisplayLogin.setForeground(new java.awt.Color(0, 0, 0));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(20, Short.MAX_VALUE)
+                .addComponent(statusDisplayLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(usernameInput, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(label2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(passwordInput, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22)
+                .addComponent(submit, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(statusDisplayLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(label1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(usernameInput, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(label2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(passwordInput)
+                        .addComponent(submit, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        submit.getAccessibleContext().setAccessibleName("");
+
+        jTableLogin.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Alarm Id", "Status", "Floor Number", "Room Number", "Smoke Level", "CO2 Level"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class, java.lang.Object.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane8.setViewportView(jTableLogin);
+
+        alertDisplayLogin.setAlignment(java.awt.Label.RIGHT);
+        alertDisplayLogin.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
+        alertDisplayLogin.setForeground(new java.awt.Color(255, 0, 51));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 193, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane8)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(labelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(alertDisplayLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 643, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane8, javax.swing.GroupLayout.PREFERRED_SIZE, 407, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(labelLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(alertDisplayLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        jMenuBar1.setBackground(new java.awt.Color(56, 128, 255));
+
+        jMenu1.setForeground(new java.awt.Color(255, 255, 255));
+        jMenu1.setText("Sensors");
+        jMenuBar1.add(jMenu1);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
+        String userName = usernameInput.getText().trim();
+        String password = passwordInput.getText().trim();
+
+        statusDisplayLogin.setText("");
+
+        String jsonString = "";
+        try {
+            jsonString = new JSONObject()
+                    .put("username", userName)
+                    .put("password", password).toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            Unirest.setTimeouts(0, 0);
+            HttpResponse<String> response = Unirest.post("http://localhost:8080/api/auth/signin")
+                    .header("Content-Type", "application/json")
+                    .body(jsonString)
+                    .asString();
+
+            JSONObject myResponse = new JSONObject(response.getBody().toString());
+            if (response.getStatus() == 200) {
+                Login.TOKEN_BEARER = myResponse.getString("accessToken");
+                Login.TOKEN_BEARER_TYPE = myResponse.getString("tokenType");
+                statusDisplayLogin.setText("OK");
+                this.setVisible(false);
+                timer.cancel();
+                new Home().setVisible(true);
+            } else {
+                statusDisplayLogin.setText("Unauthorized!");
+            }
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+
+    }//GEN-LAST:event_submitActionPerformed
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new Login().setVisible(true);
+
+                } catch (Exception ex) {
+                    Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private java.awt.Label alertDisplayLogin;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane8;
+    private javax.swing.JTable jTableLogin;
+    private java.awt.Label label1;
+    private java.awt.Label label2;
+    private java.awt.Label labelLogin;
+    private javax.swing.JPasswordField passwordInput;
+    private java.awt.Label statusDisplayLogin;
+    private java.awt.Button submit;
+    private java.awt.TextField usernameInput;
+    // End of variables declaration//GEN-END:variables
+}
