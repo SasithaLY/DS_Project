d6025f8bb923a42c0cd1bc51965ea430488313b6
it17163132@my.sliit.lk <it17163132@my.sliit.lk>
2020-04-23 22:26:19 +0530

    new folder

--- WebFrontend/src/cardLayout.js
+++ WebFrontend/src/cardLayout.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 const isDanger = (val) => {
-    if(val > 5) {
-        return {color: "#ff0000"}
+    if (val > 5) {
+        return { color: "#ff0000" }
     }
 };
+
+const isStatus = (val) => {
+    if (val) {
+        return "Active"
+    }
+    else {
+        return "Inactive"
+    }
+};
+
 
 const Card = ({ alarm }) => {
     return (
@@ -13,6 +23,7 @@ const Card = ({ alarm }) => {
                 <div className="card-header">Floor No: {alarm.floorNo} - Room No: {alarm.roomNo}</div>
                 <div className="card-body">
                     <p>Sensor Id: {alarm.sensorId}</p>
+                    <p>Status: {isStatus(alarm.active)}</p>
                     <p style={isDanger(alarm.smoke)}>Smoke Level: {alarm.smoke}</p>
                     <p style={isDanger(alarm.co2)}>CO2 Level: {alarm.co2}</p>
                 </div>
