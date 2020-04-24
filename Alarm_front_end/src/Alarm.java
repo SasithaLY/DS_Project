efdc31fefc8f5ee7c0cd5aa6ca006516f0bec59f
ishan <agishandissanayake@gmail.com>
2020-04-24 15:22:59 +0530

    Desktop App Updated!

---
+++ Alarm_front_end/src/Alarm.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * @author ishan
+ */
+public class Alarm {
+    int alarmId;
+    int status;
+    int floorNumber;
+    int roomNumber;
+    int smokeLevel;
+    int co2Level;
+
+    public int getAlarmId() {
+        return alarmId;
+    }
+
+    public void setAlarmId(int alarmId) {
+        this.alarmId = alarmId;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+
+    public void setFloorNumber(int floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getSmokeLevel() {
+        return smokeLevel;
+    }
+
+    public void setSmokeLevel(int smokeLevel) {
+        this.smokeLevel = smokeLevel;
+    }
+
+    public int getCo2Level() {
+        return co2Level;
+    }
+
+    public void setCo2Level(int co2Level) {
+        this.co2Level = co2Level;
+    }
+
+    
+    
+    
+}
